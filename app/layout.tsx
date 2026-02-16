593042f0d78ae18a28eca3cc14c59e0718b24c0d
varun.nayak <iamvarunnayak@gmail.com>
2026-02-16 14:47:36 +0530

    update analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,8 +30,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <Toaster position="bottom-right" richColors closeButton
-         />
+        <Toaster position="bottom-right" richColors closeButton />
+        <Analytics />
       </body>
     </html>
   );
